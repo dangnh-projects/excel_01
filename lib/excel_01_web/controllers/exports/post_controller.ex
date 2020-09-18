ea661d7c61f89fdb3d8306c72c9fd939c713ba94
Dang Nguyen Hai <dangnh@nhg.vn>
2020-09-18 18:20:17 +0700

    add button

--- lib/excel_01_web/controllers/exports/post_controller.ex
+++ lib/excel_01_web/controllers/exports/post_controller.ex
@@ -3,9 +3,11 @@ defmodule Excel01Web.Exports.PostController do
   alias Excel01.Posters
   alias Excel01.Src.Employee.Employees
 
-  def index(conn, _params) do
+  def index(conn, params) do
     posts = Posters.list_posts()
     employees = Employees.list_all_employees()
+    IO.inspect "------------------------------------"
+    IO.inspect params
     conn
     |> render("report.xlsx", %{posts: posts, employees: employees})
   end
